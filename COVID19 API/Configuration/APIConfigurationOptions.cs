72a5f726bd0e71ad02362540d10b4c6524cdf176
Khursheed <khursheed.er.24@gmail.com>
2022-05-29 10:13:10 +0400

    Delete COVID19 API directory

--- COVID19 API/Configuration/APIConfigurationOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace COVID19_API.Configuration
-{
-    public class APIConfigurationOptions
-    {
-
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
